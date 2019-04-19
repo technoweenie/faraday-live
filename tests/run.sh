436f79ce1d4b9f9fe9b71dc231bea57ed6f59c9e
rick olson <technoweenie@gmail.com>
2019-04-19 14:23:00 -0600

    exit early if insecure tests or mkcert install fail

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 echo "# tests with http and unverified https server"
 rspec ./spec/insecure_spec.rb
+
 echo "# installing local CA"
 /root/.local/share/mkcert/mkcert -install
+
 echo "# tests with verified https server"
 rspec ./spec/secure_spec.rb
