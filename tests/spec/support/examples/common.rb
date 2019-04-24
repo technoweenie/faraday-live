4969bc5b89f6d332a4396101ac8c29c1f1ceec34
rick olson <technoweenie@gmail.com>
2019-04-24 14:42:55 -0600

    pass :request_header option to downstream shared examples

--- tests/spec/support/examples/common.rb
+++ tests/spec/support/examples/common.rb
@@ -5,13 +5,16 @@ shared_examples 'common request tests' do |url_kind, adapter, options|
 
   describe "with #{adapter}: #GET" do
     it_behaves_like 'an idempotent request', :get, url_kind, adapter,
+      request_header: options[:request_header],
       response_header: options[:response_header]
   end if FaradayMethods.get_method?(adapter)
 
   describe "with #{adapter}: #POST" do
     it_behaves_like 'a form post request', :post, url_kind, adapter,
-      request_header: options[:request_header]
+      request_header: options[:request_header],
+      response_header: options[:response_header]
     it_behaves_like 'a multipart request', :post, url_kind, adapter,
-      request_header: options[:request_header]
+      request_header: options[:request_header],
+      response_header: options[:response_header]
   end if FaradayMethods.post_method?(adapter)
 end
