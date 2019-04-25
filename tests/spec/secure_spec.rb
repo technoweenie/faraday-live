242911abb15643fa95d816e18a7b93083a78ed56
rick olson <technoweenie@gmail.com>
2019-04-25 12:36:57 -0600

    add tests to confirm no adapters support https proxies

--- tests/spec/secure_spec.rb
+++ tests/spec/secure_spec.rb
@@ -19,4 +19,29 @@ FaradayAdapters.each do |adapter|
       auth: "faraday:live",
     }
   end if ServerProtocols.proxy?
+
+  next if adapter.key == :typhoeus # https://github.com/technoweenie/faraday-live/issues/4
+  next if adapter.key == :httpclient # https://github.com/technoweenie/faraday-live/issues/6
+
+  describe "#{adapter} using HTTPS proxy with HTTPS server" do
+    it "fails to connect" do
+      reqs_url = FaradayURLs.server(:https, "requests")
+      proxy_url = FaradayURLs.server(:https_proxy)
+      conn = Faraday.new(reqs_url, proxy: proxy_url) do |conn|
+        conn.adapter(adapter.key)
+      end
+      expect { conn.get 'wat' }.to raise_error(Faraday::ConnectionFailed)
+    end
+  end if ServerProtocols.proxy?
+
+  describe "#{adapter} using authenticated HTTPS proxy with HTTPS server" do
+    it "fails to connect" do
+      reqs_url = FaradayURLs.server(:https, "requests")
+      proxy_url = FaradayURLs.server(:https_auth_proxy, 'faraday:live')
+      conn = Faraday.new(reqs_url, proxy: proxy_url) do |conn|
+        conn.adapter(adapter.key)
+      end
+      expect { conn.get 'wat' }.to raise_error(Faraday::ConnectionFailed)
+    end
+  end if ServerProtocols.proxy?
 end
